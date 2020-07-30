5bcf989219384b922502cf863b831ef0adc74dfd
edsonphx <68605321+edsonphx@users.noreply.github.com>
2020-07-30 14:31:54 -0300

    Add files via upload

---
+++ c.py
@@ -0,0 +1,63 @@
+import os
+import pyautogui
+import time
+import schedule
+import getpass
+
+user  = getpass.getuser()
+
+code1 = ('77568020757')
+passw1 = ('vS1XB1')
+time1 = ('17:09')
+
+code2 = ('')
+passw2 = ('')
+time2 = ('00:00')
+
+code3 = ('')
+passw3 = ('')
+time3 = ('00:00')
+
+code4 = ('')
+passw4 = ('')
+time4 = ('00:00')
+
+code5 = ('')
+passw5 = ('')
+time5 = ('00:00')
+
+def class0(code, passw):
+   os.system("TASKKILL /F /IM Zoom.exe")
+   os.popen('C:/Users/{}/AppData/Roaming/Zoom/bin/Zoom.exe'.format(user))
+   time.sleep(5)
+   x,y = pyautogui.locateCenterOnScreen('join.png')
+   pyautogui.click(x, y)
+   time.sleep(5)
+   pyautogui.write(str(code))
+   pyautogui.press('enter')
+   time.sleep(5)
+   pyautogui.write(str(passw))
+   pyautogui.press('enter')
+   time.sleep(30)
+   pyautogui.click()
+   pyautogui.hotkey('alt', 'a')
+   #pyautogui.hotkey('alt', 'v')
+   #pyautogui.hotkey('alt', 'h')
+   #time.sleep(0.5)
+   #pyautogui.write('bom dia')
+   #time.sleep(0.5)
+   #pyautogui.press('Enter')
+   #time.sleep(5)
+   #pyautogui.write('meu microfone ta ruim mas eu vou ver de arrumar')
+   #time.sleep(0.5)
+   #pyautogui.press('Enter')
+ 
+schedule.every().day.at(time1).do(class0, code=code1, passw=passw1)
+schedule.every().day.at(time2).do(class0, code=code2, passw=passw2)
+schedule.every().day.at(time3).do(class0, code=code3, passw=passw3)
+schedule.every().day.at(time4).do(class0, code=code4, passw=passw4)
+schedule.every().day.at(time5).do(class0, code=code5, passw=passw5)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
